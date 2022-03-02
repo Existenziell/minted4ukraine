c3e37aeab772248b0da103a389bd7d2361c5726a
Christof <bauer.christof@gmail.com>
2022-03-02 15:11:19 -0500

    CSS

--- components/Donate.js
+++ components/Donate.js
@@ -1,6 +1,6 @@
 const Donate = () => {
   return (
-    <div className="mb-16">
+    <div>
       <h1>How to support the project?</h1>
       <h2>You can support us and the above mentioned NGOs by minting a NFT.</h2>
     </div>
