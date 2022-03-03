1d6a8bc17884cff04f5b43dfdea81b41687298fd
Christof <bauer.christof@gmail.com>
2022-03-03 14:38:45 -0500

    Better grid

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const Minted4Ukraine = ({ articles }) => {
         </div>
       </section>
 
-      <div className="max-w-2xl m-auto px-8 pb-12">
+      <div className="m-auto px-8 pb-12">
         <About />
       </div>
 
